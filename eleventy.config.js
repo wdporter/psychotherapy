410b6b8a01dbc7f5fae27faca717338346d409a8
David Porter <david.porter@fastmail.com>
2025-12-27 08:31:44 +1000

    change order of passthrough

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,6 @@
 export default (config) => {
-	config.addPassthroughCopy("src/META-INF")
-	//config.addPassthroughCopy("src/acim.css")
-	//config.addPassthroughCopy("src/metadata.opf")
 	config.addPassthroughCopy("src/mimetype")
-	//config.addPassthroughCopy("src/nav.xhtml")
+	config.addPassthroughCopy("src/META-INF")
 }
 
 export const config = {
